caffc7a05fdb0dac4e3e2458da3dfa9f4c08fe85
Xun Liu <liuxun@apache.org>
2019-11-20 19:51:29 +0800

    1

--- submarine-commons/commons-metastore/src/test/java/org/apache/submarine/commons/metastore/SubmarineMetaStoreTest.java
+++ submarine-commons/commons-metastore/src/test/java/org/apache/submarine/commons/metastore/SubmarineMetaStoreTest.java
@@ -49,18 +49,10 @@ import static org.junit.Assert.assertEquals;
 public class SubmarineMetaStoreTest {
   private static final SubmarineConfiguration submarineConf = SubmarineConfiguration.getInstance();
 
-  static {
-    submarineConf.setMetastoreJdbcUrl("jdbc:mysql://127.0.0.1:3306/metastoreDB_test?" +
-        "useUnicode=true&amp;characterEncoding=UTF-8&amp;autoReconnect=true&amp;" +
-        "failOverReadOnly=false&amp;zeroDateTimeBehavior=convertToNull&amp;useSSL=false");
-    submarineConf.setMetastoreJdbcUserName("metastore_test");
-    submarineConf.setMetastoreJdbcPassword("password_test");
-  }
-
-  private SubmarineMetaStore submarineMetaStore = new SubmarineMetaStore(submarineConf);
-
   @Before
   public void createDatabase() throws InvalidObjectException, MetaException {
+    System.out.println("createDatabase <<< ");
+
     String url = "jdbc:mysql://127.0.0.1:3306/metastoredb_test?" +
         "useUnicode=true&amp;characterEncoding=UTF-8&amp;autoReconnect=true&amp;" +
         "failOverReadOnly=false&amp;zeroDateTimeBehavior=convertToNull&amp;useSSL=false";
@@ -85,100 +77,6 @@ public class SubmarineMetaStoreTest {
       System.out.println("数据库连接失败！");
     }
 
-    Database database = new Database();
-    database.setName("testdb");
-    database.setDescription("testdb");
-    database.setLocationUri("hdfs://mycluster/user/hive/warehouse/testdb.db");
-    Map map = new HashMap();
-    map.put("key", "value");
-    database.setParameters(map);
-    database.setOwnerName("root");
-    database.setOwnerType(PrincipalType.USER);
-    submarineMetaStore.createDatabase(database);
-
-    Table table = new Table();
-    table.setTableName("testtable");
-    table.setDbName("testdb");
-    table.setOwner("root");
-    table.setCreateTime((int) new Date().getTime() / 1000);
-    table.setLastAccessTime((int) new Date().getTime() / 1000);
-    table.setRetention(0);
-    StorageDescriptor sd = new StorageDescriptor();
-    List<FieldSchema> fieldSchemas = new ArrayList<>();
-    FieldSchema fieldSchema = new FieldSchema();
-    fieldSchema.setName("a");
-    fieldSchema.setType("int");
-    fieldSchema.setComment("a");
-    fieldSchemas.add(fieldSchema);
-    sd.setCols(fieldSchemas);
-    sd.setLocation("hdfs://mycluster/user/hive/warehouse/testdb.db/testtable");
-    sd.setInputFormat("org.apache.hadoop.mapred.TextInputFormat");
-    sd.setOutputFormat("org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat");
-    sd.setCompressed(false);
-    sd.setNumBuckets(-1);
-    SerDeInfo serdeInfo = new SerDeInfo();
-    serdeInfo.setName("test");
-    serdeInfo.setSerializationLib("org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe");
-    Map<String, String> parametersMap = new HashMap();
-    parametersMap.put("serialization.format", "|");
-    parametersMap.put("field.delim", "|");
-    serdeInfo.setParameters(parametersMap);
-    sd.setSerdeInfo(serdeInfo);
-    table.setSd(sd);
-    List<FieldSchema> partitionKeys = new ArrayList<>();
-    table.setPartitionKeys(partitionKeys);
-    Map<String, String> parameters = new HashMap<>();
-    table.setParameters(parameters);
-    String viewOriginalText = "";
-    table.setViewOriginalText(viewOriginalText);
-    String viewExpandedText = "";
-    table.setViewExpandedText(viewExpandedText);
-    String tableType = "MANAGED_TABLE";
-    table.setTableType(tableType);
-    submarineMetaStore.createTable(table);
-
-    Table tableTest = submarineMetaStore.getTable("testdb", "testtable");
-    assertEquals("testtable", tableTest.getTableName());
-
+    System.out.println("createDatabase >>> ");
   }
-
-  @After
-  public void removeAllRecord() throws Exception {
-    submarineMetaStore.dropTable("testdb", "testtable");
-    int tableCount = submarineMetaStore.getTableCount();
-    assertEquals(0, tableCount);
-
-    submarineMetaStore.dropDatabase("testdb");
-    assertEquals(1, submarineMetaStore.getDatabaseCount());
-  }
-
-  @Test
-  public void getDatabaseCount() throws InvalidObjectException, MetaException {
-    assertEquals(2, submarineMetaStore.getDatabaseCount());
-  }
-
-  @Test
-  public void getAllDatabases() throws MetaException {
-    List<String> databases = submarineMetaStore.getAllDatabases();
-    assertEquals(true, databases.contains("testdb"));
-  }
-
-  @Test
-  public void getDatabase() throws NoSuchObjectException {
-    Database database = submarineMetaStore.getDatabase("testdb");
-    assertEquals("testdb", database.getName());
-  }
-
-  @Test
-  public void getAllTables() throws MetaException {
-    List<String> tables = submarineMetaStore.getAllTables("testdb");
-    assertEquals(true, tables.contains("testtable"));
-  }
-
-  @Test
-  public void getTableCount() throws MetaException {
-    int tableCount = submarineMetaStore.getTableCount();
-    assertEquals(1, tableCount);
-  }
-
 }
